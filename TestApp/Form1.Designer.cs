35b27fed8cf44262042f1153f72eeb92c28ea3e4
Bazhanova Tatyana <kamaltatyana@gmail.com>
2017-05-09 16:34:17 +0500

    .

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textArrayOrigin = new System.Windows.Forms.Label();
+            this.textResult = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textIteration = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.generation_Array);
+            // 
+            // textArrayOrigin
+            // 
+            this.textArrayOrigin.AutoSize = true;
+            this.textArrayOrigin.Location = new System.Drawing.Point(300, 27);
+            this.textArrayOrigin.Name = "textArrayOrigin";
+            this.textArrayOrigin.Size = new System.Drawing.Size(0, 13);
+            this.textArrayOrigin.TabIndex = 1;
+            // 
+            // textResult
+            // 
+            this.textResult.AutoSize = true;
+            this.textResult.ForeColor = System.Drawing.Color.Blue;
+            this.textResult.Location = new System.Drawing.Point(105, 97);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(0, 13);
+            this.textResult.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(235, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Итерации:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 97);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Результат:";
+            // 
+            // textIteration
+            // 
+            this.textIteration.AutoSize = true;
+            this.textIteration.Location = new System.Drawing.Point(300, 59);
+            this.textIteration.Name = "textIteration";
+            this.textIteration.Size = new System.Drawing.Size(0, 13);
+            this.textIteration.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(192, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(102, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Исходный массив:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(430, 181);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textIteration);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.textArrayOrigin);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сортировка массива";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label textArrayOrigin;
+        private System.Windows.Forms.Label textResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label textIteration;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void generation_Array(object sender, EventArgs e)
+        {
+            textArrayOrigin.Text = null;
+            textResult.Text = null;
+            var array = Enumerable.Range(1, 6).ToArray();
+            var rand = new Random();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                int j = rand.Next(i + 1);
+                var tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+            foreach (var numeral in array)
+            textArrayOrigin.Text += " " + numeral;       
+            start_Sort(array);           
+        }
+        private void start_Sort(int[] array)
+        {  
+            textIteration.Text = null;
+            int count = 0;
+            bool ch = true;
+            while (ch & count < 3)
+            {   
+                ch = false;
+                count = count+1;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (array[i] > array[i + 3])
+                    {
+                        textResult.Text = null;
+                        ch = true;
+                        int[] mass = new int[2];
+                        mass[0] = array[i];
+                        mass[1] = array[i + 1];
+                        array[i] = array[i + 2];
+                        array[i + 1] = array[i + 3];
+                        array[i + 2] = mass[0];
+                        array[i + 3] = mass[1];
+                        foreach (int r in array)
+                        {
+                            textResult.Text += r.ToString();
+                        }
+                        String textIter = String.Format("{0}{1} <-> {2}{3}", array[i], array[i + 1], array[i + 2], array[i + 3]);
+                        textIteration.Text += textIter + " - " + textResult.Text + "\n";
+                    } 
+                }
+            }
+            int[] mas = new int[]{1,2,3,4,5,6};
+            if (array.SequenceEqual(mas))
+            {
+                MessageBox.Show("Массив отсортирован!");
+            }
+            else
+            {
+                textResult.Text = null;
+                textResult.Text = "Массив не сортируется";           
+            }
+        } 
+    }
+}
